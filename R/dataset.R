922a71764721513af717d946a774b46490e1852f
Alejandro Verri <averri@gmail.com>
2024-08-30 11:40:56 -0300

    update fron icloud drive

--- R/dataset.R
+++ R/dataset.R
@@ -1,16 +1,16 @@
 source("R/setup.R")
+
+
+# Spectral data -----
 # MRD274 data
-ASD1<- fread("spectra/dataset/MRD274/ASD.CSV")
-DSP <- fread("spectra/dataset/MRD274/DSP.CSV")
-FOS <- fread("spectra/dataset/MRD274/FOS.CSV")
+ASD1<- fread("data-raw/spectra/dataset/MRD274/ASD.CSV")
+DSP <- fread("data-raw/spectra/dataset/MRD274/DSP.CSV")
+FOS <- fread("data-raw/spectra/dataset/MRD274/FOS.CSV")
 # BalyaNorth data
-ASD2<- fread("spectra/dataset/BalyaNorth/ASD.CSV")
-
-# Lithology
-LITH_A <- fread("data/litho_A.csv")
-LITH_B <- fread("data/litho_B_V2.csv")
+ASD2<- fread("data-raw/spectra/dataset/BalyaNorth/ASD.CSV")
 
 
+# Get data from spectrometers
 ivar <- "Wavelength_(nm)"
 mvars <- colnames(ASD1)[!colnames(ASD1) %in% ivar]
 melt(ASD1, id.vars = ivar, measure.vars = mvars, variable.name = "Sample", value.name = "Reflectance") -> ASD1
@@ -24,7 +24,7 @@ melt(DSP, id.vars = ivar, measure.vars = mvars, variable.name = "Sample", value.
 
 mvars <- colnames(FOS)[!colnames(FOS) %in% ivar]
 melt(FOS, id.vars = ivar, measure.vars = mvars, variable.name = "Sample", value.name = "Reflectance") -> FOS
-
+# Tag and Bind tables
 DATA <- rbindlist(list(
   data.table(SourceID="asd", ProjectID="mrd",ASD1),
   data.table(SourceID="asd", ProjectID="bn",ASD2),
@@ -32,17 +32,72 @@ DATA <- rbindlist(list(
   data.table(SourceID="fos", ProjectID="mrd",FOS)
   
 ))
-
+# Rename Wavelegth column to WL
+setnames(DATA, "Wavelength_(nm)", "WL")
+# Rename Reflectance to R
+setnames(DATA, "Reflectance", "R")
+# Remove shit tagged by TSG8
 DATA[,SampleID:=sub(".*:(\\w+).*", "\\1", Sample)]
 DATA[,Sample:=NULL]
-
+# Build Index
 IDX <- DATA[,.(SampleID,SourceID,ProjectID)] |> unique()
 
 
-IDX[,LithoGeochem_A:=(SampleID %in% LITH_A$SampleID) ]
-IDX[,LithoGeochem_A:=(SampleID %in% LITH_B$SampleID)]
 
-fwrite(IDX, "data/IDX.csv")
+# Litogeochemistry data ----
+# Dataset A
+# Removed from Source duplicated Ni. We kept Ni values which most values above the DL 
+# Removed from Source LithoID
 
-# Son las mismas litogeoquimicas A y B?
+LITH_A <- fread("data-raw/litho_A_v2.csv",check.names=TRUE) #1115 samples
+LITH_A[,SetID:="A"] # Tag provider
+# Remove spaces and dots from column names, LITH_A |> colnames() |> grep(pattern="[ .]",value=TRUE)
+
+OLD <- colnames(LITH_A)
+NEW <- OLD |> gsub(pattern ="[ .]", replacement="_")
+setnames(LITH_A,OLD,NEW)
+
+# Dataset B
+# Removed from Source LithoID
+
+LITH_B <- fread("data-raw/litho_B_v2.csv",check.names=TRUE) #951 samples 
+LITH_B[,SetID:="B"] # Tag provider
+
+# Removed from source Ni_4AES_pp with most values below detection limits. Use Ni_1DXMS_p instead. Anna double check
+
+# Remove suffixes from features names
+OLD <- colnames(LITH_B)
+NEW <- OLD |> sub(pattern="(_4AES.*|_1DXMS.*)$", replacement="")
+setnames(LITH_B,OLD,NEW)
+
+# Remove spaces
+OLD <- colnames(LITH_B)
+NEW <- OLD |> gsub(pattern ="[ .]", replacement="_")
+setnames(LITH_B,OLD,NEW)
+
+
+
+# How many samples in A are also in B? 776.
+intersect(LITH_A$SampleID,LITH_B$SampleID)
+
+
+# Classify and identify which samples have lithochemistry data
+IDX[,LGC:=(SampleID %in% LITH_A$SampleID & SampleID %in% LITH_B$SampleID) ]
+
+
+
+
+# Check boundaries (min,max) of all spectra by SourceID, and set the boundary between max(min(WL)) and min(max(WL))
+# 
+
+
+# Build Training datasets ----
+# Select spectral data with Lithogeochem available (733 records)
+# DT <- DATA[LGC==TRUE & ProjectID=="mrd"]
+
+
+
+# Merge IDX into DATA
+# DATA <- IDX[DATA,on=.(SourceID,ProjectID,SampleID)]
+# fwrite(DATA, "data/DATA.csv")
 
